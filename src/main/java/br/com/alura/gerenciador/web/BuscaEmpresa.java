6c63dbca02b5cc00fc69c5116d79859ac6f54ec5
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-12 11:19:03 -0400

    Aula 1 - Atividade 1 - Criando o corpo da página de resultados da busca

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -1,13 +1,13 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.text.html.HTMLWriter;
 
 @WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
@@ -15,7 +15,12 @@ public class BuscaEmpresa extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		new HTMLWriter();
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<body>");
+		writer.println("Resultado da busca:<br/>");
+		writer.println("</body>");
+		writer.println("</html>");
 	}
 
 }
