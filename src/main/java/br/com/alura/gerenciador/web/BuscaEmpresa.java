ca7d281c58211f43678524324355c1e74e15a8d8
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-12 16:48:42 -0400

    Aula 1 - Atividade 4 - Utilizando parametros via GET para realizar a busca de uma empresa

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -2,6 +2,7 @@ package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,16 +10,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
 @WebServlet(urlPatterns = "/busca")
 public class BuscaEmpresa extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String filtro = req.getParameter("filtro");
+		EmpresaDAO empresaDAO = new EmpresaDAO();
+		Collection<Empresa> resultado = empresaDAO.buscaPorSimilaridade(filtro);
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html>");
 		writer.println("<body>");
 		writer.println("Resultado da busca:<br/>");
+		writer.println("<ul>");
+		resultado.forEach(empresa -> writer
+				.println("<li>Empresa Id: " + empresa.getId() + " e nome: " + empresa.getNome() + "</li>"));
+		writer.println("</ul>");
 		writer.println("</body>");
 		writer.println("</html>");
 	}
