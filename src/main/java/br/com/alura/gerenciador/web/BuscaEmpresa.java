04ba46654af572c276e0acb0d19fc4d4bb01a291
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-12 11:13:27 -0400

    Importando os arquivos do cursto

---
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -0,0 +1,21 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.HTMLWriter;
+
+@WebServlet(urlPatterns = "/busca")
+public class BuscaEmpresa extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		new HTMLWriter();
+	}
+
+}
