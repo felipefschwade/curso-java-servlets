14a1c9e8234d512529f50dcb098d9cc063cc3119
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-20 00:20:05 -0400

    Aula 6 - Atividades 2 e 3 - Criando o método de Logout e Alterando o tempo de expiração do Cookie a cada requisicao

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = "/*")
 public class FiltroDeAuditoria implements Filter {
@@ -26,9 +27,16 @@ public class FiltroDeAuditoria implements Filter {
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		HttpServletRequest requiscao = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
 		Cookie cookie = new Cookies(requiscao.getCookies()).getUsuarioLogado();
+		String usuario = "<deslogado>";
 		String uri = requiscao.getRequestURI();
 		System.out.println("Usuario " + usuario + " acessou a URI: " + uri);
+		if (cookie != null) {
+			cookie.setMaxAge(10 * 60);
+			usuario = cookie.getValue();
+			response.addCookie(cookie);
+		}
 		chain.doFilter(req, resp);
 	}
 
