a51f9154efd3f406780296be08a7b144fa1c997f
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-07-24 14:16:44 -0400

    Aula 10 - Atividade 1  - Criando uma página dinâmica com o JSP

--- src/main/java/br/com/alura/gerenciador/web/AdicionaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/AdicionaEmpresa.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,12 +22,9 @@ public class AdicionaEmpresa extends HttpServlet {
 		EmpresaDAO empresaDAO = new EmpresaDAO();
 		Empresa empresa = new Empresa(nome);
 		empresaDAO.adiciona(empresa);
-		PrintWriter writer = resp.getWriter();
-		writer.write("<html>");
-		writer.write("<body>");
-		writer.write("A empresa: " + nome + " foi Adicionada com Sucesso!");
-		writer.write("</body>");
-		writer.write("</html>");
+		req.setAttribute("empresa", empresa);
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/novaEmpresa.jsp");
+		dispatcher.forward(req, resp);
 	}
 
 }
